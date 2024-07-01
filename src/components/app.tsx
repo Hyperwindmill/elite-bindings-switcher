78a1e24f05d32919c085fe07946f6bbdc0f8a15f
Daniele Traverso <daniele.traverso@leviahub.com>
2024-07-02 00:26:17 +0200

    prime style

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,12 +1,15 @@
 import { createRoot } from "react-dom/client";
 import { BackupView } from "./backup.view";
 import { BindingsService } from "../services/bindings.service";
+import { PrimeReactProvider } from "primereact/api";
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+import "primeflex/primeflex.css";
 
 const root = createRoot(document.body);
 const bindingsService = new BindingsService();
 root.render(
-  <>
+  <PrimeReactProvider>
     <h1>Elite Dangerous Bindings Switcher</h1>
     <BackupView service={bindingsService}></BackupView>
-  </>
+  </PrimeReactProvider>
 );
--- src/components/backup.view.tsx
+++ src/components/backup.view.tsx
@@ -1,10 +1,13 @@
-import { useEffect, useState } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import { BindingsService } from "../services/bindings.service";
+import { classNames } from "primereact/utils";
+import { Button } from "primereact/button";
+import { DataView } from "primereact/dataview";
 interface BVInput {
   service: BindingsService;
 }
 export function BackupView(options: BVInput) {
-  const [backups, setBackups] = useState([]);
+  const [backups, setBackups] = useState<Array<string>>([]);
   const loadBackups = () => {
     options.service.loadBackups().then((res) => {
       setBackups(res);
@@ -13,16 +16,40 @@ export function BackupView(options: BVInput) {
   useEffect(() => {
     loadBackups();
   }, []);
+  const itemTemplate = (backup: string, index: number) => {
+    return (
+      <div className="col-12" key={backup}>
+        <div
+          className={classNames(
+            "flex flex-column xl:flex-row xl:align-items-start p-4 gap-4",
+            { "border-top-1 surface-border": index !== 0 }
+          )}
+        >
+          <div className="flex flex-column sm:flex-row justify-content-between align-items-center xl:align-items-start flex-1 gap-4">
+            <div className="backup-name flex flex-column align-items-center sm:align-items-start gap-3">
+              {backup}
+            </div>
+            <div className="flex sm:flex-column align-items-center sm:align-items-end gap-3 sm:gap-2">
+              <Button className="p-button-rounded" label="Restore"></Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const listTemplate = (items: string[]): ReactNode[] => {
+    if (!items || items.length === 0) return null;
+
+    const list = items.map((product, index) => {
+      return itemTemplate(product, index);
+    });
+
+    return [<div className="grid grid-nogutter">{list}</div>];
+  };
   return (
     <>
-      <div>
-        <button id="list-backups">List Backups</button>
-        <ul id="backups-list">
-          {backups.map((val) => {
-            return <li>{val}</li>;
-          })}
-        </ul>
-      </div>
+      <DataView value={backups} listTemplate={listTemplate}></DataView>
       <div>
         <input type="text" id="backup-name" placeholder="Backup Name" />
         <button id="backup">Backup</button>
